d05f334482407e1833549f1a43196ad66c92e443
4Miguel2 <delgdevoleper@gmail.com>
2024-01-31 22:40:08 -0300

    Map com listas

--- src/application/MapTest01.java
+++ src/application/MapTest01.java
@@ -4,6 +4,7 @@ import entities.Consumidor;
 import entities.Manga;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapTest01 {
@@ -18,12 +19,19 @@ public class MapTest01 {
         Manga manga4 = new Manga(3L, "Attack on Titan", 11.20);
         Manga manga5 = new Manga(2L, "Dragon ball Z", 2.99);
 
-        Map<Consumidor, Manga> consumidorManga = new HashMap<>();
-        consumidorManga.put(consumidor1, manga1);
-        consumidorManga.put(consumidor2, manga4);
+        List <Manga> mangaConsumidorList1 = List.of(manga1, manga2, manga3);
+        List <Manga> mangaConsumidorList2 = List.of(manga3, manga4);
 
-        for(Map.Entry<Consumidor, Manga> entry : consumidorManga.entrySet()) {
-            System.out.println(entry.getKey().getName() + " " + entry.getKey().getId() + " : " + entry.getValue().getNome());
+        Map<Consumidor, List<Manga>> consumidorMangaMap = new HashMap<>();
+
+        consumidorMangaMap.put(consumidor1, mangaConsumidorList1);
+        consumidorMangaMap.put(consumidor2, mangaConsumidorList2);
+
+        for(Map.Entry<Consumidor, List<Manga>> entry : consumidorMangaMap.entrySet()) {
+            System.out.println(entry.getKey().getName());
+            for (Manga manga : entry.getValue()) {
+                System.out.println(manga.getNome());
+            }
             System.out.println();
         }
 
